0dfe11ac9af49a0e399170d1a406910cc58890e6
Akshat Atray <64903530+akshatatray@users.noreply.github.com>
2021-01-08 11:39:53 +0530

    Redux Setup ⚙⚙

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
 
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./Components/Home/Home";
+
 const App = () => {
-  return <div className="App">App Works!</div>;
+  return (
+    <Router>
+      <Route path="/" exact component={Home} />
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+import Navbar from "../Navbar/Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <div>Home Works</div>
+    </>
+  );
+};
+
+export default Home;
